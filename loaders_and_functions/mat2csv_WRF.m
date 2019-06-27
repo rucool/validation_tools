26dee1bbae4b218563646a65a0499bc190c5f151
Jaden Dicopoulos <jad438@scarletmail.rutgers.edu>
2019-06-27 09:44:59 -0400

    Map loader created

--- loaders_and_functions/mat2csv_WRF.m
+++ loaders_and_functions/mat2csv_WRF.m
@@ -2,11 +2,11 @@
 clear; clc;
 
 %% load
-load('temp_data/ru-wrf_v4.1_domains.mat')
+load('temp_data/ru-wrf_v4.1_domains_v3.mat')
 
-csvwrite('XLAT_M_1km_WRF41.csv',XLATM1km)
-csvwrite('XLAT_M_3km_WRF41.csv',XLATM3km)
-csvwrite('XLAT_M_9km_WRF41.csv',XLATM9km)
-csvwrite('XLONG_M_1km_WRF41.csv',XLONGM1km)
-csvwrite('XLONG_M_3km_WRF41.csv',XLONGM3km)
-csvwrite('XLONG_M_9km_WRF41.csv',XLONGM9km)
+csvwrite('XLAT_M_1km_WRF41_v3.csv',XLATM1km)
+csvwrite('XLAT_M_3km_WRF41_v3.csv',XLATM3km)
+csvwrite('XLAT_M_9km_WRF41_v3.csv',XLATM9km)
+csvwrite('XLONG_M_1km_WRF41_v3.csv',XLONGM1km)
+csvwrite('XLONG_M_3km_WRF41_v3.csv',XLONGM3km)
+csvwrite('XLONG_M_9km_WRF41_v3.csv',XLONGM9km)
